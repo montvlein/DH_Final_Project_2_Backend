0d746b4f03a4943c4c758d612b2aab144644d354
MarianaSpaccesi <marianaism.smi@gmail.com>
2023-10-26 22:03:20 -0300

    anda ok

--- event-service/src/main/java/com/dh/eventservice/domain/repository/EventRepository.java
+++ event-service/src/main/java/com/dh/eventservice/domain/repository/EventRepository.java
@@ -1,6 +1,7 @@
 package com.dh.eventservice.domain.repository;
 
 
+import com.dh.eventservice.domain.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
