5cab7381545b189bf0c30da45f94099122af561d
MarianaSpaccesi <marianaism.smi@gmail.com>
2023-10-26 22:43:56 -0300

    conflicts resolved

--- event-service/src/main/java/com/dh/eventservice/domain/model/Event.java
+++ event-service/src/main/java/com/dh/eventservice/domain/model/Event.java
@@ -1,15 +1,12 @@
 package com.dh.eventservice.domain.model;
 
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.util.Date;
 
 @Data
 @NoArgsConstructor
