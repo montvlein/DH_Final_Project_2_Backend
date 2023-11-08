4e04bd3e9d1bed40f72155ac503ea24291934edb
MarianaSpaccesi <marianaism.smi@gmail.com>
2023-11-08 13:38:49 -0300

    preparo metodos getListByCountry y getListByCountryAndCity

--- event-service/src/main/java/com/dh/eventservice/api/service/EventService.java
+++ event-service/src/main/java/com/dh/eventservice/api/service/EventService.java
@@ -11,16 +11,25 @@ import java.util.List;
 
 public interface EventService {
 
-	List<Event> getListByCategory(String category);
-    List<EventDTO> getListByCountry (String country);
+    List<Event> getListByCategory(String category);
+
+    List<EventDTO> getListByCountry(String country);
+
     List<EventDTO> getListByCountryAndCity(String country, String city);
+
     List<EventDTO> getListByCategoryId(Integer id);
+
     List<EventDTO> getListByVenue(String venue);
+
     List<EventDTO> getAllEvents();
+
     EventDTO save(EventDTO eventDto);
+
     EventDTO findById(Integer id) throws ResourceNotFoundExceptions;
+
     void delete(Integer id) throws ResourceNotFoundExceptions;
+
     String update(EventDTO eventDTO) throws ResourceNotFoundExceptions;
 
-   List<Event> getListByDate (String dateTime) throws ResourceNotFoundExceptions;
+    List<Event> getListByDate(String dateTime) throws ResourceNotFoundExceptions;
 }
