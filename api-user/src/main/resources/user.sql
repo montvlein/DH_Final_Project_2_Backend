40fe3cc2d83b764581126e06d8a88ab80ffc5137
Damián Micheli <damianmicheli@gmail.com>
2023-11-15 21:14:43 -0300

    correccion menor en script de datos de usuario

--- api-user/src/main/resources/user.sql
+++ api-user/src/main/resources/user.sql
@@ -1,5 +1,5 @@
-INSERT INTO users( first_name, last_name, mail, password,birth_Date, phone, role ,id_Type , id_Number) VALUES( 'marinaaa','zuritaaaa', 'marinaaaa@gmail.com', '12345678', '23-02-1987', 12343212, 'ADMIN', 'DNI', 32718230);
-INSERT INTO users( first_name, last_name, mail, password,birth_Date, phone, role ,id_Type , id_Number) VALUES( 'marinaaa','zuritaaaa', 'mari@gmail.com', '12345678', '23-02-1987', 12343212, 'USER', 'DNI', 32718230);
+INSERT INTO users( id, first_name, last_name, mail, password, birth_date, phone, role , document_type, document_number) VALUES(1, 'marinaaa','zuritaaaa', 'marinaaaa@gmail.com', '12345678', '23-02-1987', 12343212, 'ADMIN', 'DNI', 32718230);
+INSERT INTO users( id, first_name, last_name, mail, password, birth_date, phone, role ,document_type , document_number) VALUES(2, 'marinaaa','zuritaaaa', 'mari@gmail.com', '12345678', '23-02-1987', 12343212, 'USER', 'DNI', 32718230);
 
 
 
